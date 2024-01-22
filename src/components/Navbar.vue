95a946b9f3f790f09eb006589d05fdb71a35aa3c
Jason Huang <huangpengxiao@gmail.com>
2024-01-22 12:45:08 +0800

    pc submenu done, mobile sub nav next

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,18 +16,30 @@
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12 ml-52">
-                <div class="relative" v-for="menuItem in menuItems" :key="menuItem.ID">
-                    <a type="button" @click="toggleMenu($event)" :href="menuItem.url"
-                        class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 hover:text-darkGrayishBlue cursor-pointer"
+                <div class="relative" v-for="menuItem in topLevelMenuItems" :key="menuItem.ID">
+                    <a type="button" @click="toggleMenu(menuItem,$event)" :href="menuItem.url"
+                        class="flex items-center gap-x-1 text-base font-bold leading-6 text-gray-900 hover:text-darkGrayishBlue cursor-pointer"
                         aria-expanded="false">
                         {{ menuItem.title }}
-                        <svg v-if="hasChildren[menuItem.ID]" class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20"
+                        <svg v-if="menuItem.children?.length" class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20"
                             fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd"
                                 d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                                 clip-rule="evenodd" />
                         </svg>
                     </a>
+                    <transition name="fade">
+                        <div v-if="menuItem.isMenuVisible" :ref="getMenuRef(menuItem.ID)"
+                            class="menu absolute z-10 mt-3 p-4 flex flex-nowrap flex-col items-start gap-y-3 rounded bg-white shadow-lg ring-1 ring-gray-900/5 w-28">
+                            <!--div
+                                class="flex flex-col items-center gap-y-3 rounded p-4 text-sm leading-6 hover:bg-gray-50">
+                            </div-->
+                            <a v-for="child in menuItem.children" :href="child.url" :key="child.ID"
+                                class="text-sm font-semibold text-gray-900 hover:text-darkGrayishBlue">
+                                {{ child.title }}
+                            </a>
+                        </div>
+                    </transition>
                 </div>
 
                 <!--
@@ -40,137 +52,7 @@
             From: "opacity-100 translate-y-0"
             To: "opacity-0 translate-y-1"
             -->
-                <transition name="fade">
-                    <div v-if="isMenuVisible" ref="menu"
-                        class="menu absolute top-12 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                        <div class="p-4">
-                            <div
-                                class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                                <div
-                                    class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-                                    </svg>
-                                </div>
-                                <div class="flex-auto">
-                                    <a href="#" class="block font-semibold text-gray-900">
-                                        Analytics
-                                        <span class="absolute inset-0"></span>
-                                    </a>
-                                    <p class="mt-1 text-gray-600">
-                                        Get a better understanding of your traffic
-                                    </p>
-                                </div>
-                            </div>
-                            <div
-                                class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                                <div
-                                    class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
-                                    </svg>
-                                </div>
-                                <div class="flex-auto">
-                                    <a href="#" class="block font-semibold text-gray-900">
-                                        Engagement
-                                        <span class="absolute inset-0"></span>
-                                    </a>
-                                    <p class="mt-1 text-gray-600">
-                                        Speak directly to your customers
-                                    </p>
-                                </div>
-                            </div>
-                            <div
-                                class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                                <div
-                                    class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
-                                    </svg>
-                                </div>
-                                <div class="flex-auto">
-                                    <a href="#" class="block font-semibold text-gray-900">
-                                        Security
-                                        <span class="absolute inset-0"></span>
-                                    </a>
-                                    <p class="mt-1 text-gray-600">
-                                        Your customers’ data will be safe and secure
-                                    </p>
-                                </div>
-                            </div>
-                            <div
-                                class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                                <div
-                                    class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-                                    </svg>
-                                </div>
-                                <div class="flex-auto">
-                                    <a href="#" class="block font-semibold text-gray-900">
-                                        Integrations
-                                        <span class="absolute inset-0"></span>
-                                    </a>
-                                    <p class="mt-1 text-gray-600">
-                                        Connect with third-party tools
-                                    </p>
-                                </div>
-                            </div>
-                            <div
-                                class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                                <div
-                                    class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none"
-                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                                    </svg>
-                                </div>
-                                <div class="flex-auto">
-                                    <a href="#" class="block font-semibold text-gray-900">
-                                        Automation
-                                        <span class="absolute inset-0"></span>
-                                    </a>
-                                    <p class="mt-1 text-gray-600">
-                                        Build strategic funnels that will convert
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                            <a href="#"
-                                class="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100">
-                                <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                    aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                        d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm6.39-2.908a.75.75 0 01.766.027l3.5 2.25a.75.75 0 010 1.262l-3.5 2.25A.75.75 0 018 12.25v-4.5a.75.75 0 01.39-.658z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                Watch demo
-                            </a>
-                            <a href="#"
-                                class="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100">
-                                <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                    aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                        d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                Contact sales
-                            </a>
-                        </div>
-                    </div>
-                </transition>
+
                 <!--a href="#" class="text-sm font-semibold leading-6 text-gray-900 hover:text-darkGrayishBlue">Features</a>
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900 hover:text-darkGrayishBlue">Blog</a>
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900 hover:text-darkGrayishBlue">About US</a-->
@@ -183,7 +65,8 @@
             <div class="lg:hidden" style="z-index: 1">
                 <div id="menu"
                     :class="['absolute', 'flex-col', 'self-end', 'py-8', 'mt-10', 'items-center', 'space-y-6', 'font-bold', 'bg-white', 'sm:w-auto', 'sm:self-center', 'left-6', 'right-6', 'drop-shadow-md', { flex: isMobileMenuOpen, hidden: !isMobileMenuOpen }]">
-                    <a v-for="menuItem in menuItems" :key="menuItem.ID" :href="menuItem.url">{{ menuItem.title }}</a>
+                    <a v-for="menuItem in topLevelMenuItems" :key="menuItem.ID" :href="menuItem.url">{{ menuItem.title
+                    }}</a>
                 </div>
             </div>
         </nav>
@@ -194,10 +77,8 @@
 export default {
     data() {
         return {
-            isMenuVisible: false,
-            isMobileMenuOpen: false, // 跟踪移动设备菜单是否打开
-            menuItems: [],
-            hasChildren: {},
+            isMobileMenuOpen: false,
+            topLevelMenuItems: [],
             scrolled: false,
         };
     },
@@ -205,20 +86,10 @@ export default {
         document.addEventListener('click', this.handleOutsideClick);
         window.addEventListener('scroll', this.handleScroll);
 
-        // fetch('http://libofei.com/wp-json/techqik/v1/menu/primary' + '? t=${new Date().getTime()}')
         fetch(this.$urlWrap('http://libofei.com/wp-json/techqik/v1/menu/primary'))
             .then(response => response.json())
             .then(menuItems => {
-                // 使用菜单数据
-                this.menuItems = menuItems;
-                menuItems.forEach(item => {
-                    this.hasChildren[item.ID] = false;
-                });
-                menuItems.forEach(item => {
-                    if (item.menu_item_parent !== "0") {
-                        this.hasChildren[item.menu_item_parent] = true;
-                    }
-                });
+                this.topLevelMenuItems = this.constructMenu(menuItems);
             })
             .catch(error => console.error('Error:', error));
     },
@@ -226,23 +97,49 @@ export default {
         window.removeEventListener('scroll', this.handleScroll);
     },
     methods: {
-        toggleMenu(event) {
-            this.isMenuVisible = !this.isMenuVisible;
-            event.stopPropagation(); // 阻止事件冒泡
+        toggleMenu(menuItem,e) {
+            // Toggle visibility of the submenu
+            menuItem.isMenuVisible = !menuItem.isMenuVisible;
+
+            // Close other submenus
+            this.topLevelMenuItems.forEach(item => {
+                if (item.ID !== menuItem.ID) {
+                    item.isMenuVisible = false;
+                }
+            });
+            e.stopPropagation();
+        },
+        getMenuRef(menuItem) {
+            return 'menu-${menuItem.ID}';
         },
         handleOutsideClick(e) {
-            // 如果点击发生在子菜单之外并且菜单是可见的
-            if (this.isMenuVisible && (!this.$refs.menu || !this.$refs.menu.contains(e.target))) {
-                this.isMenuVisible = false;
-            }
+            this.topLevelMenuItems.forEach(menuItem => {
+                const menuRefs = this.$refs[this.getMenuRef(menuItem.ID)];
+                if (menuRefs) {
+                    // Since menuRefs is an array, check if any ref contains the click target
+                    const isClickOutside = menuRefs.every(ref => !ref.contains(e.target));
+                    if (isClickOutside) {
+                        menuItem.isMenuVisible = false;
+                    }
+                }
+            });
         },
         toggleMobileMenu(e) {
-            this.isMobileMenuOpen = !this.isMobileMenuOpen; // 切换移动设备菜单状态
+            this.isMobileMenuOpen = !this.isMobileMenuOpen;
         },
         handleScroll() {
             this.scrolled = window.scrollY > 0;
-            console.log(window.scrollY);
         },
+        constructMenu(menuItems) {
+            const menuStructure = {};
+            menuItems.forEach(item => menuStructure[item.ID] = { ...item, children: [] });
+            menuItems.forEach(item => {
+                if (item.menu_item_parent !== "0") {
+                    menuStructure[item.menu_item_parent].children.push(menuStructure[item.ID]);
+                }
+            });
+            return Object.values(menuStructure).filter(item => item.menu_item_parent === "0").sort((a, b) => a.menu_order - b.menu_order);
+        }
     },
     beforeDestroy() {
         document.removeEventListener('click', this.handleOutsideClick);
@@ -251,8 +148,6 @@ export default {
 </script>
 
 <style scoped>
-/* 如果有需要，这里可以添加针对 Navbar 的样式 */
-/* CSS 过渡样式 */
 .fade-enter-from,
 .fade-leave-to {
     opacity: 0;
